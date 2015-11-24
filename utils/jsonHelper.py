7568f7a0ced04013324714cdfddc96d1ef3383c9
Kyung yul Kevin Lim <thekoalaz@gmail.com>
2015-11-23 17:22:23 -0800

    Moved retrrieveData to utils. jsonHelper.loadData now shuffles data.

--- utils/jsonHelper.py
+++ utils/jsonHelper.py
@@ -1,8 +1,10 @@
-import json
+﻿import json
+import random
 
 def loadData(fileName):
     with open(fileName, 'r') as infile:
         data = json.load(infile)
+    random.shuffle(data)
     return data
 
 def saveData(fileName, data):
